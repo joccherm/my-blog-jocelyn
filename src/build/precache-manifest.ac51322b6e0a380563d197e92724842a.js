f2907f34982567fe00ad5222b60c2037f6468924
jocelynherman <joccherm@gmail.com>
2020-05-07 19:14:33 -0400

    First Jocelyn

---
+++ src/build/precache-manifest.ac51322b6e0a380563d197e92724842a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "659a61a3457f3c3861972276b2f51645",
+    "url": "/index.html"
+  },
+  {
+    "revision": "472f7233a4223ce1aae7",
+    "url": "/static/css/main.fe6824ef.chunk.css"
+  },
+  {
+    "revision": "6ac32d2e9b58823ea516",
+    "url": "/static/js/2.aa04e143.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.aa04e143.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "472f7233a4223ce1aae7",
+    "url": "/static/js/main.4eb692ae.chunk.js"
+  },
+  {
+    "revision": "053c8e34507918769709",
+    "url": "/static/js/runtime-main.aecaeffa.js"
+  }
+]);
